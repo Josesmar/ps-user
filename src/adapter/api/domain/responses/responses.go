a6fe6b2ba62d7bf0aff94cce26a1b6dd38107fc8
Josesmar <josesmargds@gmail.com>
2022-08-10 21:54:15 -0300

    feat: add Access-Control-Allow-Methods

--- src/adapter/api/domain/responses/responses.go
+++ src/adapter/api/domain/responses/responses.go
@@ -10,6 +10,8 @@ import (
 func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	w.WriteHeader(statusCode)
 
 	if data != nil {

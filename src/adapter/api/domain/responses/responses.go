9ec11a9fa425ceb3e725f5569c63f6acc98d71dd
Josesmar <josesmargds@gmail.com>
2022-08-12 09:15:36 -0300

    fix: comentando header acess-control-allow-headers

--- src/adapter/api/domain/responses/responses.go
+++ src/adapter/api/domain/responses/responses.go
@@ -12,7 +12,7 @@ func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Origin, Accept, Accept-  Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization")
+	// w.Header().Set("Access-Control-Allow-Headers", "Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization")
 
 	w.WriteHeader(statusCode)
 
